2f593e7fc026d11355e2d80d479be15c12f147fa
gerdalukosiute <lukosiute.2085763l@studenti.uniroma1.it>
2025-03-27 15:17:55 +0100

    new way to deal with replies

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -15,17 +15,17 @@ func (rt *_router) Handler() http.Handler {
 	// works with current, needs to be retested after some convos present
 	rt.router.POST("/conversations", rt.withAuth(rt.handleStartConversation)) 
 	// updated, retest aswell
-
-	rt.router.GET("/conversations/:conversationId", rt.withAuth(rt.handleGetConversationDetails))
-	// After dealing with messages
 	rt.router.POST("/conversations/:conversationId/messages", rt.withAuth(rt.handleSendMessage))
 	rt.router.GET("/media/:mediaId", rt.withAuth(rt.handleGetMedia)) // New
-	// re-do to handle both pics and texts
+	// Updated and tested
 	rt.router.PUT("/messages/:messageId/status", rt.withAuth(rt.handleUpdateMessageStatus))
 	rt.router.POST("/messages/:messageId/forward", rt.withAuth(rt.handleForwardMessage))
 	rt.router.DELETE("/messages/:messageId", rt.withAuth(rt.handleDeleteMessage))
 	rt.router.POST("/messages/:messageId/comments", rt.withAuth(rt.handleAddComment))
 	rt.router.DELETE("/messages/:messageId/comments/:commentId", rt.withAuth(rt.handleDeleteComment))
+	rt.router.POST("/messages/:messageId/replies") // New
+	rt.router.GET("/conversations/:conversationId", rt.withAuth(rt.handleGetConversationDetails))
+	// After dealing with messages
 	rt.router.GET("/groups", rt.withAuth(rt.handleGetMyGroups))
 	rt.router.POST("/groups/:groupId", rt.withAuth(rt.handleAddToGroup))
 	rt.router.DELETE("/groups/:groupId", rt.withAuth(rt.handleLeaveGroup))
